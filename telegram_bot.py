0d605e87c2577ada30c12c73bd1e7d7f99e99bd0
erikgett <erikgette63@gmail.com>
2022-11-24 23:59:28 +0500

    чутка поковырял

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,9 +5,9 @@ from telegram_elements.messages import hello_message
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text:
-        bot.send_message(message.from_user.id, hello_message)
-    keyboard_bool(message, 'Как вам данная клавиатура')
+    bot.send_message(message.from_user.id, hello_message)
+    if '/' not in message.text:
+        keyboard_bool(message, 'Как вам данная клавиатура')
 
 
 bot.polling(none_stop=True, interval=0)
