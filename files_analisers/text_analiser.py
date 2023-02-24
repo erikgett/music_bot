65e6ef3424c2f90acf0548aeb51c5d161cc57143
VictoriaVash <vash-200@mail.ru>
2023-02-24 11:48:51 +0500

    обновила по PEP8

--- files_analisers/text_analiser.py
+++ files_analisers/text_analiser.py
@@ -1,16 +1,22 @@
-import requests
 import pprint
+
+import requests
+
 from settings import hug_token
-import pytest
 
 API_URL = "https://api-inference.huggingface.co/models/bhadresh-savani/bert-base-go-emotion"
 headers = {"Authorization": f"Bearer {hug_token}"}
+
+
 def text_analis(message):
     payload = {"inputs": message}
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
+
+
 def test_text_analis():
-    assert text_analis('good')[0][0] == {'label': 'admiration', 'score': 0.7525299191474915}
+    assert text_analis('good')[0][0] == {'label': 'admiration', 'score': 0.7625299191474915}
+
+
 if __name__ == '__main__':
     pprint.pprint(text_analis('good'))
-
